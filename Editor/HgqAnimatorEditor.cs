1d2d39e4503747129c14cc896cb9a7f2af9a8318
HuangGuoqing <huangguoqing@pxsj.com>
2017-06-16 10:06:34 +0800

    Animator编辑器处理工具

---
+++ Editor/HgqAnimatorEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class HgqAnimatorEditor
+{
+
+    [MenuItem("Hgq/Animator/Remove")]
+    static void Remove()
+    {
+        if (Selection.activeObject != null && Selection.activeObject is GameObject)
+        {
+            RemoveAnimator(Selection.activeObject as GameObject);
+        }
+    }
+
+    private static void RemoveAnimator(GameObject root)
+    {
+        Animator[] animators = root.GetComponentsInChildren<Animator>();
+        for (int i = 0; i < animators.Length; i++)
+        {
+            GameObject.DestroyImmediate(animators[i]);
+        }
+    }
+}
